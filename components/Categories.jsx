7c093d392717c778cf1ee6da38cbd6ef414fe910
MVRKKSK <miryala.kautilya2020@vitbhopal.ac.in>
2021-12-27 18:17:53 +0530

    getting error in categories

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -1,20 +1,25 @@
 
-
-import { useState , useEffect } from "react"
+import Link from "next/link"
+import { useState, useEffect } from "react"
 import { getCategories } from "../services"
 
 const Categories = () => {
     const [categories, setcategories] = useState([])
+
     useEffect(() => {
-       
         return () => {
-            getCategories().then((newCategories => setcategories(newCategories)))
+            getCategories().then((newCategories) => setcategories(newCategories)
+            )
         }
     }, [])
+    console.log(categories)
     return (
-        <div>
-            {categories.map((post) => (
-                <div>{post.name}</div>
+        <div className="Categories-main shadow-lg">
+            <p className="Categories-heading">Categories</p>
+            {categories.map((category) => (
+                <Link key={category.slug} href={`/category/${category.slug}`}><div className="Categories-content"><div className="Category-slug-main">{category.name}</div></div></Link>
+
+
             ))}
         </div>
     )
--- pages/index.js
+++ pages/index.js
@@ -14,9 +14,9 @@ export default function Home({posts}) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className='row '>
-        {/* <div className='col-lg-3 d-flex flex-column align-items-center'>
+        <div className='col-lg-3 d-flex flex-column align-items-center'>
           <Categories />
-        </div> */}
+        </div>
         <div className='col-lg-6 d-flex flex-column '>
           {posts.map((post) => (
             <div>
